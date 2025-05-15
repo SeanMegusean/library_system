9c6555aa7cc4af830ab31e7d432c8390cb66a45f
Eng Bee Tin Hopia Ube Flavor <143575323+SeanMegusean@users.noreply.github.com>
2025-05-15 10:28:21 +0800

    Update library

--- sql bago/library (lahat bago).sql.sql
+++ sql bago/library (lahat bago).sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2025 at 07:25 PM
+-- Generation Time: May 15, 2025 at 04:27 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,12 +42,12 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `campus`, `title`, `author`, `year`, `quantity`, `category`) VALUES
-(1, 'San Francisco', 'Star Wars: A new Hope', 'George Clooney', 1999, 11, 'Fiction'),
-(2, 'San Francisco', 'Harry Potter: The Legend of Encantadia', 'Pepito Manaloto', 1995, 13, 'Fiction'),
+(1, 'Batasan', 'Star Wars: A new Hope', 'George Clooney', 1999, 27, 'Fiction'),
+(2, 'San Francisco', 'Harry Potter: The Legend of Encantadia', 'Pepito Manaloto', 1995, 28, 'Fiction'),
 (3, 'San Bartolme', 'IT: Chapter 69', 'Robert J. Oppenheimer', 1945, 11, 'Mystery'),
 (4, 'San Bartolme', 'Dune', 'Frank Herbert', 1965, 4, 'Sci-Fi'),
-(5, 'San Bartolme', 'Ender\'s Game', 'Orson Scott Card', 1985, 4, 'Sci-Fi'),
-(6, 'San Bartolme', 'Foundation', 'Isaac Asimov', 1951, 3, 'Sci-Fi'),
+(5, 'San Bartolme', 'Ender\'s Game', 'Orson Scott Card', 1985, 6, 'Sci-Fi'),
+(6, 'San Bartolme', 'Foundation', 'Isaac Asimov', 1951, 5, 'Sci-Fi'),
 (7, 'San Bartolme', 'Pride and Prejudice', 'Jane Austen', 1813, 4, 'Romance'),
 (8, 'San Francisco', 'Crimino and Educ', 'Filodor Dospordostoevsky', 2025, 1, 'Pyschololomo');
 
@@ -59,24 +59,29 @@ INSERT INTO `books` (`id`, `campus`, `title`, `author`, `year`, `quantity`, `cat
 
 CREATE TABLE `borrowings` (
   `id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `borrow_ref` varchar(50) DEFAULT NULL,
-  `borrow_date` date DEFAULT NULL,
   `student_number` varchar(50) DEFAULT NULL,
-  `status` varchar(20) DEFAULT NULL,
-  `return_date` date DEFAULT NULL
+  `book_id` int(11) DEFAULT NULL,
+  `borrow_ref` varchar(50) DEFAULT NULL,
+  `borrow_date` datetime DEFAULT NULL,
+  `return_date` datetime DEFAULT NULL,
+  `status` enum('Pending','Borrowed','Returned') NOT NULL DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `borrowings`
 --
 
-INSERT INTO `borrowings` (`id`, `book_id`, `borrow_ref`, `borrow_date`, `student_number`, `status`, `return_date`) VALUES
-(11, 1, '6127DA01', '2025-05-04', '12-3456', 'Returned', NULL),
-(12, 4, 'E3CCB81E', '2025-05-04', '12-3456', 'Returned', NULL),
-(13, 5, '368A1E5F', '2025-05-04', '12-3456', 'Returned', NULL),
-(14, 5, 'E28532A2', '2025-05-04', '23-2283', 'Returned', NULL),
-(15, 6, '14BD46A2', '2025-05-04', '23-2283', 'Returned', NULL);
+INSERT INTO `borrowings` (`id`, `student_number`, `book_id`, `borrow_ref`, `borrow_date`, `return_date`, `status`) VALUES
+(47, '12-3456', 2, 'E80A1E58', '2025-05-14 20:30:32', '2025-05-14 20:30:39', 'Returned'),
+(48, '12-3456', 1, 'C8665B6C', '2025-05-14 20:30:30', '2025-05-14 20:30:40', 'Returned'),
+(49, '23-4567', 2, 'CD88A149', '2025-05-14 20:31:03', '2025-05-14 20:31:31', 'Returned'),
+(50, '23-4567', 1, '4A9B9770', '2025-05-14 20:31:27', '2025-05-14 20:31:34', 'Returned'),
+(51, '23-2283', 2, 'E0B3C1EB', '2025-05-14 20:31:52', '2025-05-14 20:32:09', 'Returned'),
+(52, '23-2283', 1, '6E3B03A2', '2025-05-14 20:32:03', '2025-05-14 20:32:07', 'Returned'),
+(53, '23-2283', 5, '1F6F4933', '2025-05-14 20:32:15', '2025-05-14 20:32:53', 'Returned'),
+(54, '23-2283', 6, '2277462F', '2025-05-14 20:32:13', '2025-05-14 20:32:54', 'Returned'),
+(55, '12-3456', 5, '3C59F732', '2025-05-14 20:32:39', '2025-05-14 20:32:51', 'Returned'),
+(56, '12-3456', 6, '26E9DED1', '2025-05-14 20:32:49', '2025-05-14 20:32:52', 'Returned');
 
 -- --------------------------------------------------------
 
@@ -85,13 +90,12 @@ INSERT INTO `borrowings` (`id`, `book_id`, `borrow_ref`, `borrow_date`, `student
 --
 
 CREATE TABLE `borrowings_temp` (
-  `id` int(11) NOT NULL DEFAULT 0,
-  `book_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `student_number` varchar(20) DEFAULT NULL,
-  `borrow_ref` varchar(100) NOT NULL,
-  `borrow_date` datetime NOT NULL,
-  `status` varchar(20) DEFAULT 'Pending'
+  `id` int(11) NOT NULL,
+  `student_number` varchar(50) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `borrow_ref` varchar(50) DEFAULT NULL,
+  `borrow_date` datetime DEFAULT NULL,
+  `status` enum('Pending') DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -137,26 +141,7 @@ CREATE TABLE `mr_requests` (
 INSERT INTO `mr_requests` (`req_id`, `room_id`, `student_number`, `date_requested`, `reason`, `status`) VALUES
 (8, 1, '12-3456', '2025-05-04 08:19:07', 'mag aral', 'Approved'),
 (9, 1, '12-3456', '2025-05-04 08:46:30', 'sdasdas', 'Approved'),
-(10, 1, 'student123', '2025-05-07 20:15:11', 'idk', 'Approved'),
-(11, 1, 'student123', '2025-05-07 20:22:54', 'hell ye', 'Approved'),
-(12, 1, '12-3456', '2025-05-07 20:25:36', 'asd', 'Approved'),
-(13, 2, '12-3456', '2025-05-07 20:25:39', 'asd', 'Approved'),
-(14, 1, 'student123', '2025-05-07 20:28:44', 'asd', 'Approved'),
-(15, 1, '12-3456', '2025-05-07 20:36:04', 'z', 'Approved'),
-(16, 1, '12-3456', '2025-05-07 20:41:08', '1', 'Approved'),
-(17, 2, '12-3456', '2025-05-07 20:57:24', '1', 'Rejected'),
-(18, 1, '12-3456', '2025-05-07 21:51:40', 'zxc', 'Approved'),
-(19, 1, '23-1000', '2025-05-07 22:57:28', 'rt ddgh', 'Approved'),
-(20, 1, '12-3456', '2025-05-07 22:57:47', 'hghkghk', 'Approved'),
-(21, 4, '12-3456', '2025-05-07 22:57:57', 'gcbncvbncbn', 'Pending'),
-(22, 4, '12-3456', '2025-05-07 23:17:39', 'zxc', 'Pending'),
-(23, 4, '12-3456', '2025-05-07 23:17:44', 'zxc', 'Pending'),
-(24, 3, '12-3456', '2025-05-07 23:17:47', 'zxc', 'Pending'),
-(25, 1, '12-3456', '2025-05-07 23:18:28', 'fsd', 'Pending'),
-(26, 1, '12-3456', '2025-05-07 23:18:35', 'asd', 'Pending'),
-(27, 1, '23-1000', '2025-05-07 23:18:46', 'asd', 'Pending'),
-(28, 3, '23-1000', '2025-05-07 23:18:49', 'sad', 'Pending'),
-(29, 1, '12-3456', '2025-05-07 23:21:32', 'asd', 'Pending');
+(10, 3, '12-3456', '2025-05-14 20:37:19', 'hehe', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -173,7 +158,7 @@ CREATE TABLE `mr_reservations` (
   `end_time` time NOT NULL,
   `reason` longtext NOT NULL,
   `date_of_reservation` datetime NOT NULL,
-  `status` enum('Pending','Approved','Rejected','Cancelled') NOT NULL
+  `status` enum('Pending','Approved','Rejected','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -181,10 +166,10 @@ CREATE TABLE `mr_reservations` (
 --
 
 INSERT INTO `mr_reservations` (`res_id`, `room_id`, `student_number`, `date_reserved`, `start_time`, `end_time`, `reason`, `date_of_reservation`, `status`) VALUES
-(2, 1, '12-3456', '2025-05-05', '14:00:00', '15:00:00', 'kain lang', '2025-05-04 08:18:55', 'Cancelled'),
-(3, 2, '12-3456', '2025-05-13', '14:25:00', '02:25:00', 'asd', '2025-05-07 20:25:55', 'Cancelled'),
-(4, 1, '23-1000', '2025-05-08', '16:58:00', '16:58:00', 'fgxfgcbc', '2025-05-07 22:58:24', 'Cancelled'),
-(5, 4, '12-3456', '2025-05-08', '16:58:00', '16:58:00', 'xgcvbcbncv\r\n', '2025-05-07 22:58:48', 'Cancelled');
+(2, 1, '12-3456', '2025-05-05', '14:00:00', '15:00:00', 'kain lang', '2025-05-04 08:18:55', ''),
+(3, 2, '12-3456', '2025-05-21', '17:34:00', '18:34:00', 'adasdas', '2025-05-12 11:34:50', ''),
+(4, 1, '23-1234', '2025-05-14', '22:19:00', '23:19:00', 'la lang', '2025-05-14 16:20:03', 'Approved'),
+(5, 3, '12-3456', '2025-05-15', '02:39:00', '14:39:00', 'hehe', '2025-05-14 20:39:30', '');
 
 -- --------------------------------------------------------
 
@@ -207,17 +192,7 @@ CREATE TABLE `request_logs` (
 INSERT INTO `request_logs` (`log_id`, `req_id`, `action`, `admin_id`, `timestamp`) VALUES
 (19, 8, 'Approved', 1, '2025-05-04 06:19:37'),
 (20, 9, 'Approved', 1, '2025-05-04 06:46:57'),
-(21, 10, 'Approved', 1, '2025-05-07 18:32:58'),
-(22, 11, 'Approved', 1, '2025-05-07 18:33:03'),
-(23, 15, 'Approved', 1, '2025-05-07 18:36:11'),
-(24, 12, 'Approved', 1, '2025-05-07 18:38:52'),
-(25, 13, 'Approved', 1, '2025-05-07 18:39:04'),
-(26, 14, 'Approved', 1, '2025-05-07 18:39:20'),
-(27, 16, 'Approved', 1, '2025-05-07 18:47:21'),
-(28, 17, 'Rejected', 1, '2025-05-07 18:57:36'),
-(29, 18, 'Approved', 1, '2025-05-07 19:52:34'),
-(30, 20, 'Approved', 1, '2025-05-07 20:59:11'),
-(31, 19, 'Approved', 1, '2025-05-07 20:59:22');
+(21, 10, 'Approved', 1, '2025-05-14 18:41:34');
 
 -- --------------------------------------------------------
 
@@ -241,7 +216,11 @@ INSERT INTO `reserve_logs` (`log_id`, `res_id`, `action`, `admin_id`, `timestamp
 (1, 2, 'Approved', 1, '2025-05-07 19:09:49'),
 (2, 3, 'Approved', 1, '2025-05-07 20:52:25'),
 (3, 4, 'Approved', 1, '2025-05-07 20:59:42'),
-(4, 5, 'Approved', 1, '2025-05-07 21:00:11');
+(4, 5, 'Approved', 1, '2025-05-07 21:00:11'),
+(5, 3, 'Approved', 1, '2025-05-12 09:37:31'),
+(6, 4, 'Approved', 1, '2025-05-14 14:21:00'),
+(7, 2, 'Approved', 1, '2025-05-14 18:41:31'),
+(8, 5, 'Approved', 1, '2025-05-14 18:41:31');
 
 -- --------------------------------------------------------
 
@@ -266,7 +245,8 @@ INSERT INTO `users` (`id`, `student_number`, `full_name`, `password`, `role`) VA
 (2, '12-3456', 'Jose Mandarambong', '$2y$10$bLFarHxLFUpYlpbFNKpobuxf/3oUY3bMzh2G6OPZORLQfdVH7wQO6', 'student'),
 (3, '23-4567', 'Wally Ayolab', '$2y$10$DVdFY6zt1AnMgKHqN76G.Od6qf3Rb2paitJbRLIrw5KdjKqEP1Zce', 'student'),
 (0, '23-2283', 'Sean Fernandez', '$2y$10$/kBMUiVWuLIkOyOi2YdeHODM/XGHw9n5mAEfASchWqzUkr3Gu9M7C', 'student'),
-(0, '23-1000', 'Andro', '$2y$10$S2XloQv/kqmqSUjevwkMzu7tnQ7SGZhnbW3tMSfCMqsE9FM1FQfAG', 'student');
+(0, '23-1234', 'jonard ediwow', '$2y$10$kERPZtaxMqzuiKTUO16VneZMusZK7OfC1JcTtzWdeHuumLylrasFy', 'student'),
+(0, '23-2323', 'jonard ediwow', '$2y$10$vMDYhueoj17JY9wZhsxKy.F2dI2da.QEIOgJ46QI1laUT/hD/.M0.', 'student');
 
 --
 -- Indexes for dumped tables
@@ -276,6 +256,12 @@ INSERT INTO `users` (`id`, `student_number`, `full_name`, `password`, `role`) VA
 -- Indexes for table `borrowings`
 --
 ALTER TABLE `borrowings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `borrowings_temp`
+--
+ALTER TABLE `borrowings_temp`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -314,13 +300,19 @@ ALTER TABLE `reserve_logs`
 -- AUTO_INCREMENT for table `borrowings`
 --
 ALTER TABLE `borrowings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `borrowings_temp`
+--
+ALTER TABLE `borrowings_temp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `mr_requests`
 --
 ALTER TABLE `mr_requests`
-  MODIFY `req_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `req_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `mr_reservations`
@@ -332,13 +324,13 @@ ALTER TABLE `mr_reservations`
 -- AUTO_INCREMENT for table `request_logs`
 --
 ALTER TABLE `request_logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `reserve_logs`
 --
 ALTER TABLE `reserve_logs`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
